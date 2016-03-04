88e05093dd42af16529cd5d169fc5613c44a6e73
Andrey <andrey-kuranov@andrey-kuranov.kodep.ru>
2016-03-04 13:47:53 +0300

    fix cap#1

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,14 +53,5 @@ namespace :deploy do
     end
   end
 end
-namespace :rails do
-  desc "Tail rails logs from server"
-  task :tail_log do
-    SSHKit.config.output_verbosity = Logger::DEBUG
-    on roles(:app) do
-      execute "tail -f #{shared_path}/log/#{fetch(:rails_env)}.log"
-    end
-  end
-end
 after :deploy, 'deploy:restart'
 
