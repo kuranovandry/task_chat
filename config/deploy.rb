c7362101a50379cfe12d8bf5638170c5fca693f5
Andrey <andrey-kuranov@andrey-kuranov.kodep.ru>
2016-03-04 13:38:47 +0300

    fix log file

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ set :rvm_ruby_version, '2.2.2'
 set :linked_files, %w{config/database.yml config/secrets.yml}
 
 # Default value for linked_dirs is []
-set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system public/uploads}
+# set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets public/system public/uploads}
 set :hosts, 'hosts'
 
 namespace :deploy do
