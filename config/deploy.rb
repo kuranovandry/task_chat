74bbd417ce4a59307d93574aa7c1aa165cb23c50
Andrey <andrey-kuranov@andrey-kuranov.kodep.ru>
2016-03-04 13:28:00 +0300

    add log for capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -53,5 +53,14 @@ namespace :deploy do
     end
   end
 end
+namespace :logs do
+  desc "tail rails logs"
+  task :tail_rails do
+    on roles(:app) do
+      execute "tail -f #{shared_path}/log/#{fetch(:rails_env)}.log"
+    end
+  end
+end
+
 after :deploy, 'deploy:restart'
 
