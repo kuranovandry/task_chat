e52bea0c0dc1acb7bf6f1d5b7d261d2058ef83ad
Сафаргалиев Юрий <nekto.ryadom@gmail.com>
2016-03-22 17:53:46 +0300

    move to heizner

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,12 +2,12 @@ set :password, ask('Server password:', nil)
 set :rails_env, 'production'
 
 
-server '192.168.33.19',
+server '176.9.9.86',
   user: 'root',
   roles: %w{web app db},
   hosts: %w{host},
   ssh_options: {
-    port: 45118,
+    port: 45130,
     forward_agent: true,
         auth_methods: %w(publickey, password)
   }
