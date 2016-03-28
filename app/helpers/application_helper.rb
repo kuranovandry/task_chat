b27efdaac4bdb0eb34d320ced4c6e7fc4256607e
Andrei Malyshev <Andrei.Malyshev@kodep.ru>
2016-03-28 21:23:26 +0300

    Added automatic link conversion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,18 +1,22 @@
 module ApplicationHelper
-
   def call_comments_class(comment)
     comment.position == 1 ? 'comment_other' : 'comment_my'
   end
 
   def bootstrap_class_for(flash_type)
     case flash_type
-      when 'success'
-        "alert-success"
-      when 'error'
-        "alert-error"
-      else
-        "alert-info"
+    when 'success'
+      'alert-success'
+    when 'error'
+      'alert-error'
+    else
+      'alert-info'
     end
   end
 
+  def highlight_links(content)
+    content.gsub(%r{https?://[^\s<]+}) do |url|
+      "<a href='#{url}' target='_newtab'>#{url}</a>"
+    end.html_safe
+  end
 end
